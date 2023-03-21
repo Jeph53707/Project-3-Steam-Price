1f305fc1e605b818296c4ca04bfdbf185fd9fc9a
Scartafist <jc53707@outlook.com>
2023-03-21 16:41:04 -0400

    added form component and pseudo code | 1st commit

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+// Two Network requests
+// 1 request to get all games list | 1 map request to get detailed information if the first was successful 
+
+
+// Form that would take in user input which would then fetch the API to send us a list of all games 
+// This list of games is then placed in the Form drop down menu 
+// If the user clicks on one of the items they are given additional information
+// useEffect call to get the list of AppIDs from the API -> the list is passed down to form which will take care of   rendering each game and related information 
+// Product Info and Game Info
+
+// Do fetch call in App.js
+// Wireframe 
