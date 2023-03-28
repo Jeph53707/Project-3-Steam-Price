6a1a0c3379f34822babb6a7a3359129bc7401655
Scartafist <jc53707@outlook.com>
2023-03-28 14:40:40 -0400

    fifth commit

--- src/components/ArtPiece.js
+++ src/components/ArtPiece.js
@@ -3,7 +3,7 @@ const ArtPiece = (props) => {
         <>
             <h2>This is the ArtPiece</h2>
             <li key={props.ArtImage.id}>{props.ArtImage.title}</li>
-            <img src={props.ArtImage.webImage.url} />
+            <img src={props.ArtImage.webImage.url} alt={props.ArtImage.title}/>
         </>
     )
 }
